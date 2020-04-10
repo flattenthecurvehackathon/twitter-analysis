cbf3544e539a7d1b00cd2f7bce749eb3ea828f64
Harshil Dave <harshildave3@gmail.com>
2020-04-10 15:10:39 +1000

    data analysis v0.2

--- data-analytics-stuff/sentiment_analysis.py
+++ data-analytics-stuff/sentiment_analysis.py
@@ -1,9 +1,28 @@
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
-import matplotlib
-plt.style.use("dark_background")
 import numpy as np
+from textblob import TextBlob
+import matplotlib.pyplot as plt
+plt.style.use("dark_background")
+
+
+def get_class(polarity, labels=True):
+    """
+    Get senitment class based on polarity values
+    :param polarity:
+    :return:
+    """
+    classes = ["negative", "neutral", "positive"]
+    val = None
+    if -0.25 <= polarity <= 0.25:
+        val = 1
+    elif polarity > 0.25:
+        val = 2
+    else:
+        val = 0
+    if labels:
+        return classes[val]
+    return val
 
 
 if __name__ == '__main__':
@@ -12,18 +31,43 @@ if __name__ == '__main__':
 
     train = pd.read_csv(os.path.join(path_to_data, "train.csv"))
     test = pd.read_csv(os.path.join(path_to_data, "test.csv"))
-    print()
+
+    print(train.info())
+    print(test.info())
 
     # data exploration
-    plt.pie([len(np.where(train["sentiment"] == "negative")[0]),
-             len(np.where(train["sentiment"] == "neutral")[0]),
-             len(np.where(train["sentiment"] == "positive")[0])]
-            , labels=["Negative", "Neutral", "Positive"]
-            , explode=(0, 0.1, 0)
-            , autopct='%1.1f%%'
-            , colors=["#5597E7", "#587196", "#6A59DC"]
-            )
-    plt.title("Data Proportion")
-    plt.show()
-    plt.savefig(os.path.join(path_to_plots, "sentiment_proportion.jpg"), dpi=300)
+    fig, ax = plt.subplots()
+    ax.pie([len(np.where(train["sentiment"] == "negative")[0]),
+            len(np.where(train["sentiment"] == "neutral")[0]),
+            len(np.where(train["sentiment"] == "positive")[0])]
+           , labels=["Negative", "Neutral", "Positive"]
+           , explode=(0, 0.1, 0)
+           , autopct='%1.1f%%'
+           , colors=["#5597E7", "#587196", "#6A59DC"])
+    ax.set_title("Data Proportion")
+    fig.savefig(os.path.join(path_to_plots, "sentiment_proportion.jpg"), dpi=300)
+
+    # get preds
+    for i in range(len(train)):
+        train["prediction"] = get_class(TextBlob(train["sentiment"][i]).polarity, labels=True)
+
+    fig, ax = plt.subplots()
+    ax.pie([len(np.where(train["prediction"] == "negative")[0]),
+            len(np.where(train["prediction"] == "neutral")[0]),
+            len(np.where(train["prediction"] == "positive")[0])]
+           , labels=["Negative", "Neutral", "Positive"]
+           , explode=(0, 0.1, 0)
+           , autopct='%1.1f%%'
+           , colors=["#5597E7", "#587196", "#6A59DC"])
+    ax.set_title("Data Proportion Predictions")
+    fig.savefig(os.path.join(path_to_plots, "sentiment_prediction_proportion.jpg"), dpi=300)
+
+    train.to_csv(os.path.join(path_to_data, "predictions.csv"))
+    print(train.info())
+
+
+
+
+
+
 
